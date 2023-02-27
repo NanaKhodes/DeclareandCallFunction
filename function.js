a2cbebc1552dbd9d25656be2534939414a838e41
Nancy Ekwere <99981194+NanaKhodes@users.noreply.github.com>
2023-02-27 17:49:49 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+//How to declare a function
+function reusableFunction() {
+    console.log("Hello World");
+  }
+  reusableFunction()
+
+  //Pass values to functions with arguements
+  function functionWithArgs(a,b){
+    console.log(a +b);
+  }
+  
+  functionWithArgs(1,2)
+  functionWithArgs(7,9)
